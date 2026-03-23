fc05ac7d5764bd895807f9d24d05c7a49388a049
amans-deepsource <aman+srivastava@deepsource.io>
2026-03-23 16:46:43 +0530

    test: add unformatted JS file to test code formatter

---
+++ src/test-formatter.js
@@ -0,0 +1,18 @@
+// Unformatted file to test code formatter on PR
+const   x   =   1
+const y=2
+function   add(a,b){
+return a+b
+}
+
+const result=add(x,y)
+console.log(  "result:"  ,  result  )
+
+const arr=[1,2,3,4,5]
+const doubled=arr.map(function(item){return item*2})
+
+if(result>0){
+console.log("positive")
+}else{
+console.log("negative")
+}
